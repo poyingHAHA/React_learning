0e12b3c19a630505ca15b3531b22556c49362e56
poyingHAHA <poying.code@gmail.com>
2022-12-20 17:35:39 +0800

    2. install bulma and import css library

--- 2_reusable_components/src/App.js
+++ 2_reusable_components/src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
 // if we are adding an import for an image or for a font or really any kind of non JavaScript file, we have to add in the extension.
 import AlexaImage from './images/alexa.png'
