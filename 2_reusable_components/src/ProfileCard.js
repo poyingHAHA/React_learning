2e2d6d9b9fe475df55661f2f6f75cad5337797be
poyingHAHA <poying.code@gmail.com>
2022-12-20 17:49:13 +0800

    2. styling with bulma

--- 2_reusable_components/src/ProfileCard.js
+++ 2_reusable_components/src/ProfileCard.js
@@ -3,10 +3,19 @@ function ProfileCard({ title, handle, image }){
     // const { title, handle } = props;
 
     return (
-        <div>
-            <img src={image} alt={title} />
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt={title} />
+                </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">Title is {title}</p>
+                    <p className="subtitle is-6">Handle is {handle}</p>
+                </div>
+            </div>
         </div>
     )
 }
