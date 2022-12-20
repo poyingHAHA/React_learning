ac5082865f3aef25f1f241bb64d19b32e89c801f
poyingHAHA <poying.code@gmail.com>
2022-12-20 17:57:26 +0800

    2. finish of warm-up

--- 2_reusable_components/src/ProfileCard.js
+++ 2_reusable_components/src/ProfileCard.js
@@ -1,4 +1,4 @@
-function ProfileCard({ title, handle, image }){
+function ProfileCard({ title, handle, image, description }){
     // destructuring
     // const { title, handle } = props;
 
@@ -14,6 +14,9 @@ function ProfileCard({ title, handle, image }){
                 <div className="media-content">
                     <p className="title is-4">Title is {title}</p>
                     <p className="subtitle is-6">Handle is {handle}</p>
+                </div>
+                <div className="content">
+                    {description}
                 </div>
             </div>
         </div>
