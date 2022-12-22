bb1268a2f2f7f35edb66ae5eefec4e7649e96eb4
poyingHAHA <poying.code@gmail.com>
2022-12-22 19:31:21 +0800

    5. closing form in a nasty way

--- 5_handle_forms/src/components/BookEdit.js
+++ 5_handle_forms/src/components/BookEdit.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 
-function BookEdit({ book, onEdit }){
+function BookEdit({ book, onEdit, onSubmit }){
     const [title, setTitle] = useState(book.title);
 
     const handleChange = (event) => {
@@ -9,10 +9,8 @@ function BookEdit({ book, onEdit }){
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
         onEdit(book.id, title);
-
-        
+        onSubmit();
     }
 
     return (
