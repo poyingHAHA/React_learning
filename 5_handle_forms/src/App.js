f8f9624e2f13e80bd2613b889824c110ce3163a6
poyingHAHA <poying.code@gmail.com>
2022-12-22 16:59:18 +0800

    5. Displaying book list

--- 5_handle_forms/src/App.js
+++ 5_handle_forms/src/App.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App(){
     const [books, setBooks] = useState([]);
@@ -8,14 +9,15 @@ function App(){
         // books.push({id: 123, title: title});
         const updatedBooks = [
             ...books,
-            {id: 123, title}
+            {id: Math.round(Math.random() * 9999), title}
         ];
 
         setBooks(updatedBooks);
     }
 
     return (
-        <div>
+        <div className='app'>
+            <BookList books={books} />
             <BookCreate onCreate={handleCreateBook} />
         </div>
     )
