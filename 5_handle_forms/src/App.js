7c76859e6ed2f97bb4f9c0fcd011ada953e47567
poyingHAHA <poying.code@gmail.com>
2022-12-24 15:31:46 +0800

    5. using useEffect decising when a function should work

--- 5_handle_forms/src/App.js
+++ 5_handle_forms/src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 import axios from 'axios';
@@ -13,10 +13,19 @@ function App(){
     // DON't DO THIS
     // fetchBooks();
 
-    const editBookById = (id, newTitle) => {
+    // DO THIS
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        });
+
         const updatedBooks = books.map((book) => {
             if(book.id === id){
-                return { ...book, title: newTitle}
+                return { ...book, ...response.data}
             }
 
             return book;   
@@ -24,7 +33,9 @@ function App(){
 
         setBooks(updatedBooks);
     }
-    const deleteBookById = (id) => {
+    const deleteBookById = async(id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         })
