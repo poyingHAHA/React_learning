bccb0fc47da05723e8f0ffee0d8c2091568a3eb0
poyingHAHA <poying.code@gmail.com>
2022-12-25 15:24:29 +0800

    6. reusable component and prop-types to check the props

--- 6_custom_nav_and_routing/src/App.js
+++ 6_custom_nav_and_routing/src/App.js
@@ -1,5 +1,25 @@
+import Button from "./Button";
+
 function App(){
-    return <div>App</div>
+    return (
+        <div>
+            <div>
+                <Button success primary>Click me!!</Button>
+            </div>
+            <div>
+                <Button>Buy Now!!</Button>
+            </div>
+            <div>
+                <Button>HAHA</Button>
+            </div>
+            <div>
+                <Button>Nice Job!!</Button>
+            </div>
+            <div>
+                <Button>LoL!!</Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
