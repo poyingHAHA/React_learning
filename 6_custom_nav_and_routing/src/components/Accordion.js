cf009f539840d35bf0495d4a058444f1b571652c
poyingHAHA <poying.code@gmail.com>
2023-01-14 08:53:03 +0800

    6. adding style to the icon

--- 6_custom_nav_and_routing/src/components/Accordion.js
+++ 6_custom_nav_and_routing/src/components/Accordion.js
@@ -11,21 +11,23 @@ function Accordion({items}) {
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
-    const icon = <span>
+    const icon = <span className="text-2xl">
       {isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}
     </span>
 
     return (
       <div key={item.id}>
-        <div onClick={() => handleClick(index)}>{item.label}</div>
-        {icon}
-        {isExpanded && <div>{ item.content }</div>}
+        <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleClick(index)}>
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{ item.content }</div>}
       </div>
     )
   });
 
   return (
-    <div>
+    <div className='border-x border-t rounded'>
       {renderedItems}
     </div>
   )
