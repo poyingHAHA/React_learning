62298be6e41d3d4a39b8b8e9106857fd0eaf8594
poyingHAHA <poying.code@gmail.com>
2023-01-14 08:48:19 +0800

    6. Adding icon for accordion

--- 6_custom_nav_and_routing/src/components/Accordion.js
+++ 6_custom_nav_and_routing/src/components/Accordion.js
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import {GoChevronDown, GoChevronLeft} from "react-icons/go";
 
 function Accordion({items}) {
   const [expandedIndex, setExpandedIndex] = useState(0);
@@ -10,9 +11,14 @@ function Accordion({items}) {
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
+    const icon = <span>
+      {isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}
+    </span>
+
     return (
       <div key={item.id}>
         <div onClick={() => handleClick(index)}>{item.label}</div>
+        {icon}
         {isExpanded && <div>{ item.content }</div>}
       </div>
     )
