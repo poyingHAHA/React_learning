1fa166b61857c18fad8c6044516f1e379f445f6d
poyingHAHA <poying.code@gmail.com>
2023-01-14 21:32:49 +0800

    6. Creating the reusable Panel

--- 6_custom_nav_and_routing/src/components/Dropdown.js
+++ 6_custom_nav_and_routing/src/components/Dropdown.js
@@ -1,9 +1,10 @@
-import {useState} from 'react';
-import { GoChevronDown } from 'react-icons/go';
+import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from './Panel'; 
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
-  
+
   const handleCLick = () => {
     setIsOpen(!isOpen);
   };
@@ -13,25 +14,32 @@ function Dropdown({ options, value, onChange }) {
     setIsOpen(!isOpen);
     // WHAT OPTION DID THE USER CLICK ON??
     onChange(option);
-  }
+  };
 
   const renderedOptions = options.map((option) => {
     return (
       <div onClick={() => handleOptionClick(option)} key={option.value}>
         {option.label}
       </div>
-    )
-  })
+    );
+  });
 
   return (
     <div className="w-48 relative">
-      <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full" onClick={handleCLick} >
-        {value?.label || 'Select...'}
-        <GoChevronDown className='text-lg'/>
-      </div>
-      {isOpen && <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderedOptions}</div>}
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleCLick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
+      </Panel>
+      {isOpen && (
+        <Panel className="absolute top-full">
+          {renderedOptions}
+        </Panel>
+      )}
     </div>
-  )
+  );
 }
 
 export default Dropdown;
