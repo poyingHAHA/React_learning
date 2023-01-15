75800a4624855f818f0fa4e27e932b5fc8c2b2e8
poyingHAHA <poying.code@gmail.com>
2023-01-15 15:27:35 +0800

    7. Customizing Modal

--- 6_custom_nav_and_routing/src/pages/ModalPage.js
+++ 6_custom_nav_and_routing/src/pages/ModalPage.js
@@ -13,10 +13,18 @@ function ModalPage() {
     setShowModal(false);
   }
 
+  const actionBar = <Button onClick={handleClose} primary>I Accept</Button>
+
+  const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+    <p>
+      Here is an important agreement for you to accept
+    </p>
+  </Modal>;
+
   return (
     <div>
       <Button onClick={handleClick} primary>Open Modal</Button>
-      { showModal && <Modal onClose={handleClose} />}
+      { showModal && modal }
     </div>
   )
 }
