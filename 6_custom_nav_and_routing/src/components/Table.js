733c24b4c056e0bde54511f7e46b4a9d7589e621
poyingHAHA <poying.code@gmail.com>
2023-01-15 21:18:07 +0800

    8. customized header

--- 6_custom_nav_and_routing/src/components/Table.js
+++ 6_custom_nav_and_routing/src/components/Table.js
@@ -1,11 +1,19 @@
+// fragment is like a very, very simple custom component that we can use.
+// Just we can assign things like a key prop or group, different child elements together, stuff like.
+import {Fragment} from 'react';
+
 function Table({ data, config, keyFn }) {
   const renderedHeaders = config.map((column) => {
+    if(column.header) {
+      return <th key={column.label}>{column.header()}</th>;
+    }
+
     return <th key={column.label}>{column.label}</th>;
   });
 
   const renderRows = data.map((rowData) => {
     const renderedCells = config.map((column) => {
-      return <td className="p-2" key={column.label}>{column.render(rowData)}</td>;
+      return <Fragment className="p-2" key={column.label}>{column.render(rowData)}</Fragment>;
     })
 
     return (
