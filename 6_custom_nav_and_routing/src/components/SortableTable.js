baa6575e9873fa8ff12559e30203a449745d5b1a
poyingHAHA <poying.code@gmail.com>
2023-01-15 22:21:26 +0800

    8. Adding Sort State

--- 6_custom_nav_and_routing/src/components/SortableTable.js
+++ 6_custom_nav_and_routing/src/components/SortableTable.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import Table from "./Table";
 
 function SortableTable(props) {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
   const {config} = props;
 
   const handleCLick = (label) => {
-    console.log(label);
+    if(sortOrder === null){
+      setSortOrder('asc');
+      setSortBy(label)
+    } else if(sortOrder === 'asc'){
+      setSortOrder('desc')
+      setSortBy(label)
+    } else if(sortOrder === 'desc'){
+      setSortOrder(null);
+      setSortBy(null);
+    }
   }
 
   const updatedConfig = config.map((column) => {
@@ -19,7 +31,10 @@ function SortableTable(props) {
   });
 
   // the config property that is present inside of props will be overwritten.
-  return <Table {...props} config={updatedConfig} />; 
+  return <div>
+    {sortOrder} - {sortBy}
+    <Table {...props} config={updatedConfig} />; 
+  </div>
 }
 
 export default SortableTable;
