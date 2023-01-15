618f570af0c3dfbd2a5d455aaf1e761779a62c8a
poyingHAHA <poying.code@gmail.com>
2023-01-15 13:09:48 +0800

    6. Link component

--- 6_custom_nav_and_routing/src/App.js
+++ 6_custom_nav_and_routing/src/App.js
@@ -1,10 +1,11 @@
 import "./index.css";
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
+import Link from './components/Link';
 
 function App(){
   return (
     <div>
+      <Link to="/accordion">Go to accordion</Link>
+      <Link to="/dropdown">Go to dropdown</Link>
     </div>
   )
 }
