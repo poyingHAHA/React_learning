6a1df3062bb888c703bacc7e89d069426fd0dcbe
poyingHAHA <poying.code@gmail.com>
2023-01-15 16:16:59 +0800

    8. creating unreusable component

--- 6_custom_nav_and_routing/src/components/Table.js
+++ 6_custom_nav_and_routing/src/components/Table.js
@@ -1,5 +1,30 @@
-function Table() {
-  return <div>Table</div>;
+function Table({data}) {
+  const renderRows = data.map((fruit) => {
+    return(
+      <tr className="border-b" key={fruit.name}>
+        <td className="p-3">{fruit.name}</td>
+        <td className="p-3">
+          <div className={`p-3 m-2 ${fruit.color}`}></div>
+        </td>
+        <td className="p-3">{fruit.score}</td>
+      </tr>
+    )
+  })
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
