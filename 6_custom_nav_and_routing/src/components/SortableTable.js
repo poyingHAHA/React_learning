964aeb09903f6fb0e3fa5b6cca4fbc344149acef
poyingHAHA <poying.code@gmail.com>
2023-01-15 22:36:02 +0800

    8. finish sorting function

--- 6_custom_nav_and_routing/src/components/SortableTable.js
+++ 6_custom_nav_and_routing/src/components/SortableTable.js
@@ -4,37 +4,61 @@ import Table from "./Table";
 function SortableTable(props) {
   const [sortOrder, setSortOrder] = useState(null);
   const [sortBy, setSortBy] = useState(null);
-  const {config} = props;
+  const { config, data } = props;
 
   const handleCLick = (label) => {
-    if(sortOrder === null){
-      setSortOrder('asc');
-      setSortBy(label)
-    } else if(sortOrder === 'asc'){
-      setSortOrder('desc')
-      setSortBy(label)
-    } else if(sortOrder === 'desc'){
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
       setSortOrder(null);
       setSortBy(null);
     }
-  }
+  };
 
   const updatedConfig = config.map((column) => {
-    if(!column.sortValue){
+    if (!column.sortValue) {
       return column;
     }
 
     return {
       ...column,
-      header: () => <th onClick={() => handleCLick(column.label)}>{column.label} is sortable</th>
-    }
+      header: () => (
+        <th onClick={() => handleCLick(column.label)}>
+          {column.label} is sortable
+        </th>
+      ),
+    };
   });
 
-  // the config property that is present inside of props will be overwritten.
-  return <div>
-    {sortOrder} - {sortBy}
-    <Table {...props} config={updatedConfig} />; 
-  </div>
+  // Only sort data if sortOrder && sortBy are not null
+  // Make a copy of the 'data' prop
+  // Find the correct sortValue function and use it for sorting
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a,b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+      if(typeof valueA === 'string'){
+        return valueA.localeCompare(valueB) * reverseOrder;
+      }else{
+        return (valueA - valueB) * reverseOrder;
+      }
+    })
+  }
+
+  return (
+    <div>
+      {sortOrder} - {sortBy}
+      <Table {...props} data={sortedData} config={updatedConfig} />;
+    </div>
+  );
 }
 
 export default SortableTable;
