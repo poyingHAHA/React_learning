fe6b9d066f9ac5586041f6317b364bc6cae8f7c0
poyingHAHA <poying.code@gmail.com>
2023-01-15 12:53:06 +0800

    6. Listening to forward and Back click

--- 6_custom_nav_and_routing/src/context/navigation.js
+++ 6_custom_nav_and_routing/src/context/navigation.js
@@ -1,11 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProvider({ children }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener('popstate', handler);
+
+    return () => {
+      window.removeEventListener('popstate', handler);
+    }
+  }, [])
+
   // So whatever we put inside those curly braces is a value we're sharing with the rest of our application.
   return (
     <NavigationContext.Provider value={{}}>
+      {currentPath}
       {children}
     </NavigationContext.Provider>
   );
