8770a9c5cdfa2bbc9118fddc65c6d6f59cc1635f
poyingHAHA <poying.code@gmail.com>
2023-01-16 10:56:40 +0800

    10. Adding new state updates

--- 6_custom_nav_and_routing/src/pages/CounterPage.js
+++ 6_custom_nav_and_routing/src/pages/CounterPage.js
@@ -4,6 +4,8 @@ import Panel from "../components/Panel";
 
 const INCREMENT_COUNT = "increment";
 const SET_VALUE_TO_ADD = "change-value-to-add";
+const DECREMENT_COUNT = 'decrement';
+const ADD_VALUE_TO_COUNT = 'add_value_to_count'
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -17,36 +19,42 @@ const reducer = (state, action) => {
         ...state,
         valueToAdd: action.payload,
       };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0
+      }
     default:
       return state;
   }
 };
 
 function CounterPage({ initialCount }) {
-  // const [ count, setCount ] = useState(initialCount);
-  // const [valueToAdd, setValueToAdd] = useState(0);
   const [state, dispatch] = useReducer(reducer, {
     count: initialCount,
     valueToAdd: 0,
   });
 
   const increment = () => {
-    // setCount(count + 1);
     dispatch({
       type: INCREMENT_COUNT,
     });
   };
 
   const decrement = () => {
-    // setCount(count - 1);
     dispatch({
-      type: "decrement",
+      type: DECREMENT_COUNT,
     });
   };
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
-    // setValueToAdd(value);
     dispatch({
       type: SET_VALUE_TO_ADD,
       payload: value,
@@ -55,9 +63,9 @@ function CounterPage({ initialCount }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    // setCount(count + valueToAdd);
-    // setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    })
   };
 
   return (
