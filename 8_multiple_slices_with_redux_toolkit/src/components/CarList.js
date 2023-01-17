799f90f50ea5a72fea4cd6716685b1df8b5d60fc
poyingHAHA <poying.code@gmail.com>
2023-01-17 09:23:48 +0800

    12. Lsting the Records

--- 8_multiple_slices_with_redux_toolkit/src/components/CarList.js
+++ 8_multiple_slices_with_redux_toolkit/src/components/CarList.js
@@ -4,10 +4,29 @@ function CarList(){
   const cars = useSelector((state) => {
     return state.cars.data;
   })
-  console.log(cars);
+
+  const handleCarDelete = (car) => {
+    // ...
+  }
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button className="button is-danger" onClick={() => handleCarDelete(car)}>
+          Delete
+        </button>
+      </div>
+    )
+  })
 
   return (
-    <div>CarList</div>
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
   )
 }
 
