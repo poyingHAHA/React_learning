bd7b2efe993cac25f0befc9c607144fdc77e707d
poyingHAHA <poying.code@gmail.com>
2023-01-18 11:33:05 +0800

    13. refactoring component

---
+++ 9_api_using_async_thunks/src/components/UsersListitem.js
@@ -0,0 +1,11 @@
+function UsersListitem({ user }) {
+  return (
+    <div key={user.id} className="mb-2 border rounded">
+      <div className="flex p-2 justify-between items-center cursor-pointer">
+        {user.name}
+      </div>
+    </div>
+  );
+}
+
+export default UsersListitem
