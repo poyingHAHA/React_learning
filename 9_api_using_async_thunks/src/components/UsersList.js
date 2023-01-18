81d8fecc9d0fd5806e8dff16abd0894c69da2694
poyingHAHA <poying.code@gmail.com>
2023-01-18 09:03:08 +0800

    13. render list of users

--- 9_api_using_async_thunks/src/components/UsersList.js
+++ 9_api_using_async_thunks/src/components/UsersList.js
@@ -26,10 +26,18 @@ function UsersList() {
     )
   }
 
+  const renderUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    )
+  })
+
   return(
-    <div>
-      {data.length}
-    </div>
+    <div>{renderUsers}</div>
   )
 }
 
