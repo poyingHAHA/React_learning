cf3a1d50aef1d22c5c5bd0c6653cedde928d9232
poyingHAHA <poying.code@gmail.com>
2023-02-11 16:49:28 +0800

    s4-5 RTK QUERY add

--- redux4_RTK_Query/rtk_query_client/src/components/TodosList.tsx
+++ redux4_RTK_Query/rtk_query_client/src/components/TodosList.tsx
@@ -1,5 +1,5 @@
 import { Todo, todoApi } from "../features/api/todoApi";
-import { useCallback } from "react";
+import { useCallback, useRef } from "react";
 
 const TodosList = () => {
   // 使用剛才的 getAll query
@@ -8,7 +8,10 @@ const TodosList = () => {
   const [ updateTodo ] = todoApi.useUpdateTodoMutation();
   // 這裡使用剛才的 deleteTodo
   const [ deleteTodo ] = todoApi.useDeleteTodoMutation();
+  // 這裡使用剛才的 addTodo
+  const [ addTodo ] = todoApi.useAddTodoMutation();
 
+  const addTextRef = useRef<HTMLInputElement>(null);
   const onToggle = useCallback((todo: Todo) => {
     updateTodo({...todo, done: !todo.done})
   }, [updateTodo]);
@@ -16,6 +19,11 @@ const TodosList = () => {
   const onDelete = useCallback((todo: Todo) => {
     deleteTodo(todo)
   }, [deleteTodo])
+
+  const onAdd = useCallback(() => {
+    addTodo(addTextRef.current!.value ?? '');
+    addTextRef.current!.value = '';
+  }, [addTodo])
 
   return(
     <div className="card" style={{margin: '1rem 0'}}>
@@ -28,6 +36,10 @@ const TodosList = () => {
           <button onClick={() => onDelete(todo)}>delete</button>
         </div>
       ))}
+      <div>
+        <input type="text" ref={addTextRef} />
+        <button onClick={onAdd}>add</button>
+      </div>
     </div>
   )
 }
