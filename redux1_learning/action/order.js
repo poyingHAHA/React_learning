b5b5f00d5964f7d4505bd74211d6a1d686c24651
poyingHAHA <poying.code@gmail.com>
2023-02-11 10:05:47 +0800

    s6 - fix bugs & add cake action

--- redux1_learning/action/order.js
+++ redux1_learning/action/order.js
@@ -1,4 +1,4 @@
-const { COFFEE_ORDERED } = require("./types")
+const { COFFEE_ORDERED, COFFEEBEAN_ORDERED, CAKE_ORDERED } = require("./types")
 
 // 透過function還可以帶參數來設定，如下
 const orderCoffee = (qty, income) => {
@@ -21,7 +21,18 @@ const orderCoffeeBean = (qty, income) => {
   }
 }
 
+const orderCake = (qty, income) => {
+  return {
+    type: CAKE_ORDERED,
+    payload: {
+      qty: qty,
+      income: income
+    }
+  }
+}
+
 module.exports = {
   orderCoffee,
-  orderCoffeeBean
+  orderCoffeeBean,
+  orderCake
 }
--- redux1_learning/reducer/orderReducer.js
+++ redux1_learning/reducer/orderReducer.js
@@ -1,4 +1,11 @@
-const { COFFEE_ORDERED, COFFEE_RESTOCKED } = require("../action/types")
+const {
+  COFFEE_ORDERED,
+  COFFEE_RESTOCKED,
+  COFFEEBEAN_ORDERED,
+  COFFEEBEAN_RESTOCKED,
+  CAKE_ORDERED,
+  CAKE_RESTOCKED
+} = require("../action/types");
 
 // 2. Reducer部分(店員)
 // 先建立initialState
@@ -7,41 +14,55 @@ const initialState = {
   numOfCoffeeBean: 20,
   numOfCake: 20,
   assets: 1000,
-}
+};
 
 const orderReducer = (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case COFFEE_ORDERED:
-      // 顧客買 => 商品-, 營收+ 
+      // 顧客買 => 商品-, 營收+
       return {
         ...state,
         numOfCoffee: state.numOfCoffee - action.payload.qty,
-        assets: state.assets + action.payload.income
-      }
+        assets: state.assets + action.payload.income,
+      };
     case COFFEE_RESTOCKED:
       // 補貨 => 商品+, 營收-
       return {
         ...state,
         numOfCoffee: state.numOfCoffee + action.payload.qty,
-        assets: state.assets - action.payload.pay
-      }
-      case COFFEEBEAN_ORDERED:
-        // 顧客買 => 商品-, 營收+ 
-        return {
-          ...state,
-          numOfCoffeeBean: state.numOfCoffeeBean - action.payload.qty,
-          assets: state.assets + action.payload.income
-        }
-      case COFFEEBEAN_RESTOCKED:
-        // 補貨 => 商品+, 營收-
-        return {
-          ...state,
-          numOfCoffeeBean: state.numOfCoffeeBean + action.payload.qty,
-          assets: state.assets - action.payload.pay
-        }
-    default: 
+        assets: state.assets - action.payload.pay,
+      };
+    case COFFEEBEAN_ORDERED:
+      // 顧客買 => 商品-, 營收+
+      return {
+        ...state,
+        numOfCoffeeBean: state.numOfCoffeeBean - action.payload.qty,
+        assets: state.assets + action.payload.income,
+      };
+    case COFFEEBEAN_RESTOCKED:
+      // 補貨 => 商品+, 營收-
+      return {
+        ...state,
+        numOfCoffeeBean: state.numOfCoffeeBean + action.payload.qty,
+        assets: state.assets - action.payload.pay,
+      };
+    case CAKE_ORDERED:
+      // 顧客買 => 商品-, 營收+
+      return {
+        ...state,
+        numOfCake: state.numOfCake - action.payload.qty,
+        assets: state.assets + action.payload.income,
+      };
+    case CAKE_RESTOCKED:
+      // 補貨 => 商品+, 營收-
+      return {
+        ...state,
+        numOfCake: state.numOfCake + action.payload.qty,
+        assets: state.assets - action.payload.pay,
+      };
+    default:
       return state;
   }
-}
+};
 
-module.exports = { orderReducer }
+module.exports = { orderReducer };
