6e5c5b125ce510e7bd51f812376836d5f003c246
poyingHAHA <poying.code@gmail.com>
2023-02-11 10:10:04 +0800

    s7 - 拆分reducer

--- redux1_learning/reducer/index.js
+++ redux1_learning/reducer/index.js
@@ -1,9 +1,15 @@
 // 使用 redux 的 combineReducers function 來整合所有定義好的 reducer 檔案
 const { combineReducers } = require("redux");
-const { orderReducer } = require('./orderReducer')
+const { assetsReducer } = require("./assetsReducer");
+const { cakeReducer } = require("./cakeReducer");
+const { coffeeBeanReducer } = require("./coffeeBeanReducer");
+const { coffeeReducer } = require("./coffeeReducer");
 
 const reducers = combineReducers({
-  orderReducer,
+  coffee: coffeeReducer,
+  coffeeBean: coffeeBeanReducer,
+  cake: cakeReducer,
+  money: assetsReducer,
 })
 
 module.exports = {
